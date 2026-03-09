10451f5ee11e13d8555fa4223e433cb961522364
RazvanIonutIancu <riiancu94@gmail.com>
2026-03-09 16:19:27 +0100

    Advancing

---
+++ scripts/Mob.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+
+public partial class Mob : Node3D
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void OnPlayerEnterArea(Node3D player)
+    {
+
+    }
+
+    public void OnPlayerExitArea(Node3D player)
+    {
+
+    }
+
+    public void OnPlayerHit(Node3D player)
+    {
+
+    }
+
+    public void OnPlayerJumpOnHead(Node3D player)
+    {
+
+    }
+
+}
